5dd2f76ca618f61649c903fed3b4375a1f98d14f
sam <51648328+SamDTech@users.noreply.github.com>
2022-03-27 17:49:03 +0100

    building the sidebar

--- src/components/FriendSidebar.tsx
+++ src/components/FriendSidebar.tsx
@@ -1,22 +1,29 @@
-import { styled } from '@mui/system'
-import React from 'react'
-import AddFriendButton from './AddFriendButton'
+import { styled } from "@mui/system";
+import React from "react";
+import AddFriendButton from "./AddFriendButton";
+import FriendsList from "./FriendsList";
+import FriendsTitle from "./FriendsTitle";
+import PendingInvitaionsList from "./PendingInvitaionsList";
 
-const MainContainer = styled('div')({
-        width: "224px",
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        backgroundColor: "#2f3136",
-})
+const MainContainer = styled("div")({
+  width: "224px",
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  backgroundColor: "#2f3136",
+});
 
 const FriendSidebar = () => {
   return (
     <MainContainer>
-        <AddFriendButton />
+      <AddFriendButton />
+      <FriendsTitle title="Private Messages" />
+      <FriendsList />
+      <FriendsTitle title="Invitations" />
+      <PendingInvitaionsList />
     </MainContainer>
-  )
-}
+  );
+};
 
-export default FriendSidebar
+export default FriendSidebar;
---
+++ src/components/PendingInvitaionsList.tsx
@@ -0,0 +1,17 @@
+import { styled } from "@mui/system";
+import React from "react";
+
+const MainContainer = styled("div")({
+  height: "22%",
+  width: "100%",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  overflow: "auto",
+});
+
+const PendingInvitaionsList = () => {
+  return <MainContainer></MainContainer>;
+};
+
+export default PendingInvitaionsList;
