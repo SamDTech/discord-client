d59252928a48c0bdb09e1aca09593e0f10293124
sam <51648328+SamDTech@users.noreply.github.com>
2022-04-03 20:46:43 +0100

    building the messenger content page

--- src/components/MessengerContent.tsx
+++ src/components/MessengerContent.tsx
@@ -1,9 +1,32 @@
+import { Typography } from "@mui/material";
+import { styled } from "@mui/system";
+import React, { useEffect } from "react";
+import Messages from "./Messages";
+import NewMessageInput from "./NewMessageInput";
+
+const Wrapper = styled("div")({
+  flexGrow: 1,
+  // height: "100%",
+  // alignItems: "center",
+  // justifyContent: "center",
+  // display: "flex",
+});
+
 interface IMessengerProps {
   chosenChat: any;
 }
 
 const MessengerContent: React.FC<IMessengerProps> = ({ chosenChat }) => {
-  return <div>hello</div>;
+  useEffect(() => {
+    // TODO: fetching chat history for the chosen chat
+  }, [chosenChat]);
+
+  return (
+    <Wrapper>
+      <Messages />
+      <NewMessageInput />
+    </Wrapper>
+  );
 };
 
 export default MessengerContent;
