481e12fcba9f0ac25627b00d840c557f1a4058f6
sam <51648328+SamDTech@users.noreply.github.com>
2022-04-07 16:48:17 +0100

    rendering single message

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,71 +1,77 @@
-import React from 'react';
-import {styled} from "@mui/system";
+import React from "react";
+import { styled } from "@mui/system";
+import Avartar from "./Avartar";
+import { Typography } from "@mui/material";
 
 interface IMessageProps {
-    _id: string;
-    content: string;
-    sameAuthor: boolean,
-    author: {
-        username: string,
-    },
-    date: string,
-    sameDay: boolean,
-
+  _id: string;
+  content: string;
+  sameAuthor: boolean;
+  author: {
+    username: string;
+  };
+  date: string;
+  sameDay: boolean;
 }
 
-const MainContainer = styled('div')({
-    width: '97%',
-    display: 'flex',
-    flexDirection: 'column',
-    marginTop: '10px',
-
-
+const MainContainer = styled("div")({
+  width: "97%",
+  display: "flex",
+  flexDirection: "column",
+  marginTop: "10px",
 });
 
-const AvatarContainer = styled('div')({
-    width: '70px',
-
+const AvatarContainer = styled("div")({
+  width: "70px",
 });
 
-const MessageContainer = styled('div')({
-    display: 'flex',
-    flexDirection: 'column',
-
+const MessageContainer = styled("div")({
+  display: "flex",
+  flexDirection: "column",
 });
 
-const MessageContent = styled('div')({
-    color: "#dcddde"
-
+const MessageContent = styled("div")({
+  color: "#dcddde",
 });
 
-const SameAuthorMessageContent = styled('span')({
-    marginLeft: "70px"
-
+const SameAuthorMessageContent = styled("div")({
+  color: "#dcddde",
+  width: "97%",
 });
 
-const SameAuthorMessageText = styled('span')({
-    marginLeft: "70px"
-
+const SameAuthorMessageText = styled("span")({
+  marginLeft: "70px",
 });
-
 
 const Message: React.FC<{ message: IMessageProps }> = ({
-                                                           message: {
-                                                               _id, content, sameAuthor, author, date, sameDay
-                                                           }
-                                                       }) => {
-    if (sameAuthor && sameDay) {
-        return (
-            <SameAuthorMessageContent>
-                <SameAuthorMessageText>{content}</SameAuthorMessageText>
-            </SameAuthorMessageContent>
-        )
-    }
+  message: { _id, content, sameAuthor, author, date, sameDay },
+}) => {
+  if (sameAuthor && sameDay) {
     return (
-        <div>
-            message
-        </div>
+      <SameAuthorMessageContent>
+        <SameAuthorMessageText>{content}</SameAuthorMessageText>
+      </SameAuthorMessageContent>
     );
+  }
+  return (
+    <MainContainer>
+      <AvatarContainer>
+        <Avartar username={author.username} />
+      </AvatarContainer>
+      <MessageContainer>
+        <Typography
+          sx={{
+            fontSize: "16px",
+            color: "#fff",
+          }}
+        >
+          {author.username}{" "}
+          <span style={{ fontSize: "12px", color: "#72767d" }}>{date}</span>
+        </Typography>
+        <MessageContent>{content}</MessageContent>
+      </MessageContainer>
+    </MainContainer>
+  );
 };
 
 export default Message;
