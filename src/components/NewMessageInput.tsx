860305c3514aed3bbeaa32f15bd2939309705dbc
sam <51648328+SamDTech@users.noreply.github.com>
2022-04-08 13:29:32 +0100

    Update NewMessageInput.tsx

--- src/components/NewMessageInput.tsx
+++ src/components/NewMessageInput.tsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { styled } from "@mui/system";
+
+const MainContainer = styled("div")({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  width: "100%",
+  height: "60px",
+});
+
+const Input = styled("input")({
+  width: "100%",
+  height: "98%",
+  border: "none",
+  fontSize: "14px",
+  padding: "0 10px",
+  borderRadius: "8px",
+  color: "white",
+  backgroundColor: "#2f3136",
+});
 
 const NewMessageInput = () => {
-  return (
-    <div>NewMessageInput</div>
-  )
-}
+  const [message, setMessage] = useState("");
 
-export default NewMessageInput
+  const { chosenChatDetails } = useSelector((state: any) => state.chat);
+
+  const handleKeyPressed = (e: any) => {
+    if (e.key === "Enter") {
+      if (message.trim() !== "") {
+        handleSendMessage();
+        setMessage("");
+      }
+    }
+  };
+
+  const handleSendMessage = () => {
+    if (message.trim() !== "") {
+      const messageObject = {
+        message: message,
+        sender: "me",
+        timestamp: new Date().toLocaleString(),
+      };
+      chosenChatDetails.messages.push(messageObject);
+      setMessage("");
+    }
+  };
+
+  return (
+    <MainContainer>
+      <Input
+        placeholder={`Write message to ${chosenChatDetails.name}`}
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        onKeyDown={handleKeyPressed}
+      />
+    </MainContainer>
+  );
+};
+
+export default NewMessageInput;
